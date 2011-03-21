d3706d0e6f60f11c1172753890963084e67b10ab
erwin.coumans <erwin.coumans@08e121b0-ae19-0410-a57b-3be3395fd4fd>
2011-03-21 19:29:51 +0000

    branches/PhysicsEffects, fix some copyright license headers

--- branches/PhysicsEffects/sample/api_physics_effects/99_bullet_highlevel/physics_func.h
+++ branches/PhysicsEffects/sample/api_physics_effects/99_bullet_highlevel/physics_func.h
@@ -1,8 +1,18 @@
-﻿/* SCE CONFIDENTIAL
- * PSP2 Programmer Tool Runtime Library Release 00.930.000
- *                Copyright (C) 2010 Sony Computer Entertainment Inc.
- *                                                All Rights Reserved.
- */
+﻿/*
+Physics Effects Copyright(C) 2011 Sony Computer Entertainment Inc.
+All rights reserved.
+
+Physics Effects is open software; you can redistribute it and/or
+modify it under the terms of the BSD License.
+
+Physics Effects is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the BSD License for more details.
+
+A copy of the BSD License is distributed with
+Physics Effects under the filename: physics_effects_license.txt
+*/
 
 #ifndef __PHYSICS_FUNC_H__
 #define __PHYSICS_FUNC_H__

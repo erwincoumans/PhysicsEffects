6d5ca49498f050d49768d1443902873ea98e98ea
erwin.coumans <erwin.coumans@08e121b0-ae19-0410-a57b-3be3395fd4fd>
2011-03-21 19:29:51 +0000

    branches/PhysicsEffects, fix some copyright license headers

--- include/BulletPhysicsEffects/btBulletPhysicsEffectsWorld.cpp
+++ include/BulletPhysicsEffects/btBulletPhysicsEffectsWorld.cpp
@@ -1,9 +1,19 @@
-/* [SCE CONFIDENTIAL DOCUMENT]
- * $PSLibId$
- *                Copyright (C) 2010 Sony Computer Entertainment Inc.
- *                                                All Rights Reserved.
- */
- 
+/*
+Physics Effects Copyright(C) 2011 Sony Computer Entertainment Inc.
+All rights reserved.
+
+Physics Effects is open software; you can redistribute it and/or
+modify it under the terms of the BSD License.
+
+Physics Effects is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the BSD License for more details.
+
+A copy of the BSD License is distributed with
+Physics Effects under the filename: physics_effects_license.txt
+*/
+
 #include "btBulletPhysicsEffectsWorld.h"
 #include "BulletDynamics/Dynamics/btRigidBody.h"
 #include "BulletDynamics/ConstraintSolver/btTypedConstraint.h"
